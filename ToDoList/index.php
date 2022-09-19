fe54350b36effafa5780df13fa6444e8e9b1a20c
RodrigoGorostiague <rodrigorostiague@gmail.com>
2022-09-19 02:42:40 -0300

    Se realiza primera maquetacion y se inicia con el enrutamiento, promer commit

---
+++ ToDoList/index.php
@@ -0,0 +1,49 @@
+<?php 
+    function home(){
+        $titulo = "To Do List";
+    
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> <?php echo $titulo ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1> <?php echo $titulo ?> </h1>
+
+    <div class="conteiner">
+        <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center active">
+                Tarea 1
+                <span class="badge bg-secondary badge-pill">pill1</span>
+            </li>
+    </ul>   
+    </div>
+    <div class="conteiner">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Titulo</label>
+    <input type="text" class="form-control" id="exampleInputEmail1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Descripcion</label>
+    <input type="text" class="form-control">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Comppletada</label>
+  </div>
+  <button type="submit" class="btn btn-outline-primary">Agreagr tarea</button>
+</form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<?php } ?>
+
---
+++ ToDoList/route.php
@@ -0,0 +1,7 @@
+<?php 
+    require_once 'index.php';
+
+    if ($_GET['action'] == ''){
+        home();
+    }
+?>
